5fc229b76e5785db9f1157b68be370d94632588e
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2025-11-14 15:52:19 -0500

    Remove more stuff

--- gmailfilter/config.go
+++ gmailfilter/config.go
@@ -2,39 +2,21 @@ package gmailfilter
 
 import (
 	"context"
-	"log"
-	"net/http"
 
-	"golang.org/x/oauth2"
-	googleoauth "golang.org/x/oauth2/google"
 	"google.golang.org/api/gmail/v1"
 )
 
 // Config is the configuration structure used to instantiate the Google
 // provider.
 type Config struct {
-	client           *http.Client
-	gmailService     *gmail.Service
-	context          context.Context
-	terraformVersion string
-	userAgent        string
-
-	tokenSource oauth2.TokenSource
+	gmailService *gmail.Service
 }
 
 func (c *Config) LoadAndValidate(ctx context.Context) error {
-
 	gmailService, err := gmail.NewService(ctx)
 	if err != nil {
-		return nil
+		return err
 	}
 	c.gmailService = gmailService
 	return nil
-}
-
-func (c *Config) getTokenSource(clientScopes []string) (oauth2.TokenSource, error) {
-	// Use Application Default Credentials (ADC)
-	log.Printf("[INFO] Authenticating using Application Default Credentials (ADC)...")
-	log.Printf("[INFO]   -- Scopes: %s", clientScopes)
-	return googleoauth.DefaultTokenSource(context.Background(), clientScopes...)
 }
--- gmailfilter/provider.go
+++ gmailfilter/provider.go
@@ -23,26 +23,12 @@ func Provider() *schema.Provider {
 	}
 
 	provider.ConfigureContextFunc = func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-		terraformVersion := provider.TerraformVersion
-		if terraformVersion == "" {
-			// Terraform 0.12 introduced this field to the protocol
-			// We can therefore assume that if it's missing it's 0.10 or 0.11
-			terraformVersion = "0.11+compatible"
+		config := Config{}
+		if err := config.LoadAndValidate(ctx); err != nil {
+			return nil, diag.FromErr(err)
 		}
-		return providerConfigure(ctx, d, provider, terraformVersion)
+		return &config, nil
 	}
 
 	return provider
-}
-
-func providerConfigure(ctx context.Context, d *schema.ResourceData, p *schema.Provider, terraformVersion string) (interface{}, diag.Diagnostics) {
-	config := Config{
-		terraformVersion: terraformVersion,
-	}
-
-	if err := config.LoadAndValidate(ctx); err != nil {
-		return nil, diag.FromErr(err)
-	}
-
-	return &config, nil
 }
